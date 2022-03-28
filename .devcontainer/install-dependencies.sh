ec817e406554d6a0be82c7ae28b36b2f7ee8b14b
Jony Vesterman Cohen <jony.cohenjo@gmail.com>
2022-03-28 17:53:03 +0300

    change task installation method

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -74,15 +74,16 @@ echo "Installing go-task…"
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b $TOOL_DEST
 
 # Install kubebuilder
-os=$(go env GOOS)
-arch=$(go env GOARCH)
-kubebuilder_version=3.1.0
-echo "Installing kubebuilder ${kubebuilder_version} ($os $arch)…"
-curl -L "https://github.com/kubernetes-sigs/kubebuilder/releases/download/v${kubebuilder_version}/kubebuilder_${kubebuilder_version}_${os}_${arch}.tar.gz" | tar -xz -C /tmp/
-mv "/tmp/kubebuilder_${kubebuilder_version}_${os}_${arch}" "$KUBEBUILDER_DEST"
+echo "Installing kubebuilder…"
+# os=$(go env GOOS)
+# arch=$(go env GOARCH)
+# kubebuilder_version=3.1.0
+# echo "Installing kubebuilder ${kubebuilder_version} ($os $arch)…"
+# curl -L "https://github.com/kubernetes-sigs/kubebuilder/releases/download/v${kubebuilder_version}/kubebuilder_${kubebuilder_version}_${os}_${arch}.tar.gz" | tar -xz -C /tmp/
+# mv "/tmp/kubebuilder_${kubebuilder_version}_${os}_${arch}" "$KUBEBUILDER_DEST"
 
 # Install yq
 echo "Installing yq…"
-yq_version=v4.13.0
-yq_binary=yq_linux_amd64
-wget "https://github.com/mikefarah/yq/releases/download/${yq_version}/${yq_binary}.tar.gz" -O - | tar -xz -C "$TOOL_DEST" && mv "$TOOL_DEST/$yq_binary" "$TOOL_DEST/yq"
+# yq_version=v4.13.0
+# yq_binary=yq_linux_amd64
+# wget "https://github.com/mikefarah/yq/releases/download/${yq_version}/${yq_binary}.tar.gz" -O - | tar -xz -C "$TOOL_DEST" && mv "$TOOL_DEST/$yq_binary" "$TOOL_DEST/yq"
